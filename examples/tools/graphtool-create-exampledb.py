c94eaebfff746976fa572f12f2c2829211878131
egeland <egeland@1fda6f16-4416-0410-bfd6-867a04880151>
2007-03-23 14:12:11 +0000

    Added example
    git-svn-id: svn://t2.unl.edu/brian/GraphTool@712 1fda6f16-4416-0410-bfd6-867a04880151

---
+++ examples/tools/graphtool-create-exampledb.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from pysqlite2 import dbapi2 as sqlite
+import time
+import random
+import os
+import math
+
+con = sqlite.connect(os.environ["GRAPHTOOL_USER_ROOT"]+"/example.db")
+cur = con.cursor()
+
+cur.execute('''create table pirate (id INTEGER, name varchar(100) ) ''')
+cur.execute('''create table treasure (timebin integer, pirate integer, gold integer, jewels integer)''')
+
+pirates = ({ 'id': 0, 'name': 'John' },
+           { 'id': 1, 'name': 'Jack'},
+           { 'id': 2, 'name': 'Robert'})
+
+insert = '''insert into pirate (id, name) values (:id, :name)'''
+for pirate in pirates:
+    cur.execute(insert, pirate)
+
+insert = '''insert into treasure (timebin, pirate, gold, jewels) values (:timebin, :pirate, :gold, :jewels)'''
+
+t_end = time.time()
+t = t_end - 365*24*3600
+while t < t_end:
+    for pirate in pirates:
+        gold = 100 * random.random()
+        jewels = int(math.floor(100 * random.random()))
+        binds = { 'timebin': t, 'pirate': pirate['id'], 'gold': gold, 'jewels': jewels }
+        print "At %s pirate %s got %s gold and %s jewels" % (t, pirate, gold, jewels)
+        cur.execute(insert, binds)
+    t += 24*3600
+
+con.commit()
