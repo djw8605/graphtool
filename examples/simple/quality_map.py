c12b438e743f897fc03ca828cb764382638605d1
brian <brian@1fda6f16-4416-0410-bfd6-867a04880151>
2007-05-10 13:00:27 +0000

    Added quality map example.
    git-svn-id: svn://t2.unl.edu/brian/GraphTool@859 1fda6f16-4416-0410-bfd6-867a04880151

---
+++ examples/simple/quality_map.py
@@ -0,0 +1,30 @@
+import time, os, random, datetime
+from matplotlib.dates import num2date
+from graphtool.graphs.common_graphs import QualityMap
+from graphtool.tools.common import expand_string
+
+span = 3600
+
+# Generate our time series
+def make_time_data( ):
+    end_time = time.time(); end_time -= end_time % span
+    begin_time = end_time - 24*span
+    data = {}
+    for i in range(begin_time, end_time, span):
+        data[i] = random.random()
+    return begin_time, end_time, data
+
+
+# Bar graph stuff.
+QM = QualityMap()
+full_data = {}
+for i in range(10):
+    team_name = 'Team %i' % i
+    begin_time, end_time, data = make_time_data()
+    full_data[team_name] = data
+    
+metadata = {'title':'Quality Plot w.r.t. Time', 'starttime':begin_time, 
+            'endtime':end_time, 'span':span }
+filename = expand_string('$HOME/tmp/quality_map.png',os.environ)
+file = open( filename, 'w' )
+QM( full_data, file, metadata )
